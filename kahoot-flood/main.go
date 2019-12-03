f697054ac8a28a2746518bfad27ed06b8fa39d00
john676 <58081123+john676@users.noreply.github.com>
2019-12-03 08:07:21 -0700

    Update main.go

--- kahoot-flood/main.go
+++ kahoot-flood/main.go
@@ -17,8 +17,8 @@ const ConcurrencyCount = 4
 
 func main() {
 	if len(os.Args) != 3 && len(os.Args) != 4 {
-		fmt.Fprintln(os.Stderr, "Usage: flood <game pin> <nickname prefix> <count>")
-		fmt.Fprintln(os.Stderr, "       flood <game pin> <name_list.txt>")
+		fmt.Fprintln(os.Stderr, "Usage: flood <8398832> <joe mamma>")
+		fmt.Fprintln(os.Stderr, "       flood <8398832> <name_list.txt>")
 		os.Exit(1)
 	}
 
